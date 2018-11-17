21fc6c7095c755167185869f6b8f69a1020cd5c6
optimalstrategy <17456182+OptimalStrategy@users.noreply.github.com>
2018-11-16 20:19:17 -0500

    added Django integration

---
+++ examples/django-example/app/validators.py
@@ -0,0 +1,14 @@
+from decimal import Decimal
+from qval import Validator
+
+purchase_factories = {
+    "price": Decimal,
+    "item_id": int,
+    "token": None
+}
+
+purchase_validators = {
+    "price": Validator(lambda x: x > 0),
+    "token": Validator(lambda x: len(x) == 12),
+    "item_id": Validator(lambda x: x >= 0),
+}
