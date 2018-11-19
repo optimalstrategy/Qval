59a9e806ce5c8ee2674aaeca4ec1c571c1a97248
optimalstrategy <17456182+OptimalStrategy@users.noreply.github.com>
2018-11-18 23:02:20 -0500

    Implemented framework tests

--- tests/test_frameworks/api_test_functions.py
+++ tests/test_frameworks/api_test_functions.py
@@ -20,14 +20,14 @@ def test_divide_parameters_validated(server):
     url = f"{base}/divide"
     r = requests.get(f"{url}?a=10&b=0")
     assert r.status_code == 400
-    assert r.json()['error'] == "Invalid `b` value: 0."
+    assert r.json()["error"] == "Invalid `b` value: 0."
 
 
 def test_divide_success(server):
     url = f"{base}/divide"
     r = requests.get(f"{url}?a=10&b=5")
     assert r.status_code == 200
-    assert r.json()['answer'] == 2
+    assert r.json()["answer"] == 2
 
 
 def test_pow_parameters_required(server):
@@ -38,12 +38,51 @@ def test_pow_parameters_required(server):
 
 
 def test_pow_parameters_validated(server):
-    url = f"{base}/divide"
+    url = f"{base}/pow"
     assert requests.get(f"{url}?a=string&b=str").status_code == 400
 
 
 def test_pow_overflow_error(server):
-    url = f"{base}/divide"
+    url = f"{base}/pow"
     r = requests.get(f"{url}?a=2.2324&b=30000000")
     assert r.status_code == 500
-    # assert r.json()["error"] == ''
+    assert (
+        r.json()["error"]
+        == "An error occurred while processing you request. Please contact the website administrator."
+    )
+
+
+def test_pow_success(server):
+    url = f"{base}/pow"
+    r = requests.get(f"{url}?a=2&b=10")
+    assert r.status_code == 200
+    assert r.json()["answer"] == 1024
+
+
+def test_purchase_parameters_required(server):
+    url = f"{base}/purchase"
+    params = {"item_id=1", "price=4.2", "token=123456789012"}
+    for p1 in params:
+        for p2 in params - {p1}:
+            assert requests.get(f"{url}?{p1}&{p2}").status_code == 400
+
+
+def test_purchase_parameters_validated(server):
+    url = f"{base}/purchase"
+    params = [
+        ("-10", "123456789012", "13"),
+        ("7723", "12345678901", "71"),
+        ("3921", "123456789012", "0"),
+    ]
+    for p0, p1, p2 in params:
+        assert (
+            requests.get(f"{url}?itemd_id={p0}&token={p1}&price={p2}").status_code
+            == 400
+        )
+
+
+def test_purchase_success(server):
+    url = f"{base}/purchase"
+    r = requests.get(f"{url}?item_id=10&token=123456789012&price=4.04")
+    assert r.status_code == 200
+    assert r.json()["success"] == "Item '10' has been purchased. Check: 4.12$."
--- tests/test_frameworks/test_plain_django.py
+++ tests/test_frameworks/test_plain_django.py
@@ -6,11 +6,11 @@ from test_frameworks.common import start_server
 from test_frameworks.api_test_functions import *
 
 
-# Common tests from api_test_function will be tested using flask app
+# Common tests from api_test_function will be tested using django app
 @pytest.fixture(scope="module")
 def server(request):
-    proc = start_server("flask")
+    proc = start_server("django")
     request.addfinalizer(proc.kill)
     # Wait while server is setting up
-    time.sleep(0.3)
+    time.sleep(0.5)
     return proc
