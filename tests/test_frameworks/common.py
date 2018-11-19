9bfcde110d7a6a7bad0b676df4a0e7132b8e2648
OptimalStrategy <george@usan-podgornov.com>
2018-11-18 23:02:20 -0500

    Implemented framework tests

--- tests/test_frameworks/common.py
+++ tests/test_frameworks/common.py
@@ -1,30 +1,9 @@
 # coding: utf-8
 import os
-import signal
 from subprocess import Popen, PIPE
 
 BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 EX_DIR = os.path.join(BASE_DIR, "examples")
-
-
-# TODO: google how to do it properly
-class SupervisorProxy(Popen):
-    """
-    Wrapper over the `Popen` class.
-    Attempts to kill the process on `__delete__()` call.
-    """
-    def __del__(self, *args, **kwargs):
-        """
-        Kills the process.
-        """
-        super().__del__(*args, **kwargs)
-        try:
-            os.killpg(os.getpgid(self.pid), signal.SIGTERM)
-        except ProcessLookupError:
-            pass
-
-    def __str__(self) -> str:
-        return f'<Process: `{self.args}`'
 
 
 def execute(command: str) -> Popen:
@@ -35,7 +14,7 @@ def execute(command: str) -> Popen:
     :return: Popen instance
     """
     cmd = f"cd {EX_DIR} && {command}"
-    proc = SupervisorProxy(cmd, shell=True, stdout=PIPE, preexec_fn=os.setsid)
+    proc = Popen(cmd, shell=True, stdout=PIPE)
     return proc
 
 
@@ -45,17 +24,18 @@ def start_server(framework: str) -> Popen:
     Supports django, flask and falcon at the moment.
 
     :param framework: framework name
-    :return: SupervisorProxy object. Basically Popen,
-    but kills the process at the end.
+    :return: opened process
     """
     cmd = f"PYTHONPATH={BASE_DIR}"
     if framework == "django":
         cmd = f"cd django-example && {cmd} python manage.py runserver localhost:8000"
-    elif framework == 'flask':
+    elif framework == "flask":
         cmd = f"{cmd} FLASK_APP=flask-example.py flask run --port=8000"
-    elif framework == 'falcon':
+    elif framework == "falcon":
         cmd = f"{cmd} python falcon-example.py"
     else:
-        raise ValueError(f"Unknown framework: {framework}, "
-                         f"expected `django`, `flask` or `falcon`")
+        raise ValueError(
+            f"Unknown framework: {framework}, "
+            f"expected `django`, `flask` or `falcon`."
+        )
     return execute(cmd)
--- tests/test_frameworks/test_falcon.py
+++ tests/test_frameworks/test_falcon.py
@@ -6,10 +6,10 @@ from test_frameworks.common import start_server
 from test_frameworks.api_test_functions import *
 
 
-# Common tests from api_test_function will be tested using flask app
+# Common tests from api_test_function will be tested using falcon app
 @pytest.fixture(scope="module")
 def server(request):
-    proc = start_server("flask")
+    proc = start_server("falcon")
     request.addfinalizer(proc.kill)
     # Wait while server is setting up
     time.sleep(0.3)
