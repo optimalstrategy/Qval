a0c6875d7a30aa54b5d2d1a0d781ee3957d6235f
George <george@usan-podgornov.com>
2018-11-28 21:01:56 -0500

    fixed t y p o

--- tests/test_frameworks/test_falcon.py
+++ tests/test_frameworks/test_falcon.py
@@ -6,7 +6,7 @@ from test_frameworks.common import start_server
 from test_frameworks.api_test_functions import *
 
 
-# Common tests from api_test_function will be tested using falcon app
+# Common tests from `api_test_functions` will be runned on the falcon server
 @pytest.fixture(scope="module")
 def server(request):
     proc = start_server("falcon")
